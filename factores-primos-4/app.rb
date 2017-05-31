8c8df5cf54398a438b8aa2c6ad512649d63df1a4
User <hernancorteshc@hotmail.com>
2017-05-31 17:11:29 -0300

    Refactor de texto de Bad Request

--- factores-primos-4/app.rb
+++ factores-primos-4/app.rb
@@ -10,12 +10,12 @@ get '/primos' do
   	presentador = Presentador.new
 
   	lista_de_factores_primos = buscador.buscar_factores_primos(numero_ingresado.to_i)
-  	presentador = presentador.mostrar_listado_ascendente(lista_de_factores_primos)
+  	presentador = presentador.mostrar_listado_descendente(lista_de_factores_primos)
   	texto_a_mostrar = presentador.join(', ')
   	body texto_a_mostrar
   else
   	status 400
-  	body "bad request"
+  	body "400 Bad Request"
   end
 end
 
@@ -27,12 +27,12 @@ post '/primos' do
   	presentador = Presentador.new
 
   	lista_de_factores_primos = buscador.buscar_factores_primos(numero_ingresado.to_i)
-  	presentador = presentador.mostrar_listado_descendente(lista_de_factores_primos)
+  	presentador = presentador.mostrar_listado_ascendente(lista_de_factores_primos)
   	texto_a_mostrar = presentador.join(', ')
   	body texto_a_mostrar
   else
   	status 400
-  	body "bad request"
+  	body "400 Bad Request"
   end
 
 end
